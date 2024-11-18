14854c6608108a2572947d69f5f70151805392c8
u_m1mkt <sowtame@gmail.com>
2024-11-18 13:07:46 +0300

    "background_color": "#EF3124",

--- app/manifest.ts
+++ app/manifest.ts
@@ -7,7 +7,7 @@ export default function manifest(): MetadataRoute.Manifest {
     description: 'A Progressive Web App built with Next.js',
     start_url: '/',
     display: 'standalone',
-    background_color: '#ffffff',
+    background_color: '#EF3124',
     theme_color: '#000000',
     icons: [
       {
