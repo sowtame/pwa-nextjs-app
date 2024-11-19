6607b174121b5b5ef0762debf73319217137d5fd
u_m1mkt <sowtame@gmail.com>
2024-11-19 15:37:00 +0300

    tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <meta content="width=device-width, initial-scale=1, viewport-fit=cover" name="viewport"></meta>
+      <meta name="apple-mobile-web-app-capable" content="true" />
       <meta content="black-translucent" name="apple-mobile-web-app-status-bar-style"></meta>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
     </html>
