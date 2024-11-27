48a7c65ae19d6bdb49d62a47ec3d08ced7545014
u_m1mkt <sowtame@gmail.com>
2024-11-27 14:35:12 +0700

    lint

--- components/setup-worker/index.tsx
+++ components/setup-worker/index.tsx
@@ -1,48 +1,47 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+// import React, { useEffect, useState } from 'react'
 
-type Props = {}
+export const SetupWorker = () => {
+  // const [first, setFirst] = useState(false)
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setFirst(true)
+  //   }, 1000)
 
-export const SetupWorker = ({}: Props) => {
-  const [first, setFirst] = useState(false)
-  useEffect(() => {
-    setTimeout(() => {
-      setFirst(true)
-    }, 1000)
+  //   console.log(2)
 
-    console.log(2)
+  //   const test = async () => {
+  //     const wakeLock = await navigator.wakeLock.request('screen')
 
-    const test = async () => {
-      const wakeLock = await navigator.wakeLock.request('screen')
+  //     setTimeout(() => {
+  //       wakeLock.release()
+  //     }, 2000)
 
-      setTimeout(() => {
-        wakeLock.release()
-      }, 2000)
+  //     wakeLock.addEventListener('release', () => {
+  //       console.log('clear')
+  //     })
+  //   }
 
-      wakeLock.addEventListener('release', () => {
-        console.log('clear')
-      })
-    }
+  //   test()
 
-    test()
+  //   // if ('serviceWorker' in navigator) {
+  //   //   registerServiceWorker()
+  //   // }
+  // }, [])
 
-    // if ('serviceWorker' in navigator) {
-    //   registerServiceWorker()
-    // }
-  }, [])
+  // async function registerServiceWorker() {
+  //   const registration = await navigator.serviceWorker.register('/sw.js', {
+  //     scope: '/',
+  //     updateViaCache: 'none',
+  //   })
+  //   // const sub = await registration.pushManager.getSubscription()
+  // }
 
-  async function registerServiceWorker() {
-    const registration = await navigator.serviceWorker.register('/sw.js', {
-      scope: '/',
-      updateViaCache: 'none',
-    })
-    // const sub = await registration.pushManager.getSubscription()
-  }
-
-  if (first) {
-    return <div>auth</div>
-  }
+  // if (first) {
+  //   return <div>auth</div>
+  // }
 
   return <div></div>
 }
