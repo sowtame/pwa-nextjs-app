14e2ad1c0769e0524d17bc6453010b8dad302c6a
u_m1mkt <sowtame@gmail.com>
2025-03-07 13:21:21 +0700

    feat(deploy_kube_feature): skip_e2e deploy_kube_feature

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: 'https://alfabank.servicecdn.ru/icons/click/icon_192x192.png',
+        src: 'https://alfaonline.servicecdn.ru/public/s3/static/newclick/pwa/icons/192_192.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: 'https://alfabank.servicecdn.ru/icons/click/icon_512x512.png',
+        src: 'https://alfaonline.servicecdn.ru/public/s3/static/newclick/pwa/icons/512_512.png',
         sizes: '512x512',
         type: 'image/png',
       },
--- public/sw.js
+++ public/sw.js
@@ -1,34 +1,60 @@
-self.addEventListener('install', function (event) {
-  console.log('🚀 ~ event:', event)
-})
+// const PRECACHE = 'precache-v1'
+// const RUNTIME = 'runtime'
 
-self.addEventListener('activate', (event) => {
-  console.log('🚀 ~ self.addEventListener ~ event:', event)
-})
+// // A list of local resources we always want to be cached.
+// const PRECACHE_URLS = []
 
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', (event) => {
-  console.log('🚀 ~ self.addEventListener ~ event:', event)
-})
+// // The install handler takes care of precaching the resources we always need.
+// self.addEventListener('install', (event) => {
+//   event.waitUntil(
+//     caches
+//       .open(PRECACHE)
+//       .then((cache) => cache.addAll(PRECACHE_URLS))
+//       .then(self.skipWaiting())
+//   )
+// })
 
-// let timeToLogOut = null
+// // The activate handler takes care of cleaning up old caches.
+// self.addEventListener('activate', (event) => {
+//   const currentCaches = [PRECACHE, RUNTIME]
+//   event.waitUntil(
+//     caches
+//       .keys()
+//       .then((cacheNames) => {
+//         return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName))
+//       })
+//       .then((cachesToDelete) => {
+//         return Promise.all(
+//           cachesToDelete.map((cacheToDelete) => {
+//             return caches.delete(cacheToDelete)
+//           })
+//         )
+//       })
+//       .then(() => self.clients.claim())
+//   )
+// })
 
-// setInterval(async () => {
-//   const allClients = await self.clients.matchAll()
-//   if (!allClients[0]) {
-//     console.log('WINDOW CLOSED')
+// // The fetch handler serves responses for same-origin resources from a cache.
+// // If no response is found, it populates the runtime cache with the response
+// // from the network before returning it to the page.
+// self.addEventListener('fetch', (event) => {
+//   // Skip cross-origin requests, like those for Google Analytics.
+//   if (event.request.url.startsWith(self.location.origin)) {
+//     event.respondWith(
+//       caches.match(event.request).then((cachedResponse) => {
+//         if (cachedResponse) {
+//           return cachedResponse
+//         }
 
-//     timeToLogOut = new Date()
+//         return caches.open(RUNTIME).then((cache) => {
+//           return fetch(event.request).then((response) => {
+//             // Put a copy of the response in the runtime cache.
+//             return cache.put(event.request, response.clone()).then(() => {
+//               return response
+//             })
+//           })
+//         })
+//       })
+//     )
 //   }
-// }, 1000)
-
-// setInterval(async () => {
-//   // console.log('🚀 ~ setInterval ~ allClients:', allClients[0])
-//   // console.log('Hello world from the Service Worker 🤙')
-
-//   if(timeToLogOut){
-
-//   }
-// }, 500)
+// })
