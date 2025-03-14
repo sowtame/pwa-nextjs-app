369c44bffc9b048f716c342fe82832166fdde3aa
u_m1mkt <sowtame@gmail.com>
2025-03-14 15:11:51 +0700

    feat(deploy_kube_feature): skip_e2e deploy_kube_feature

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,8 +11,45 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: './LogoPWA_192_192.png',
+        src: 'https://alfaonline.servicecdn.ru/public/s3/static/newclick/pwa/icons/android/36_36.png',
+        sizes: '36x36',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: 'https://alfaonline.servicecdn.ru/public/s3/static/newclick/pwa/icons/android/48_48.png',
+        sizes: '48x48',
+        type: 'image/png',
+        density: 1,
+        purpose: 'maskable',
+      },
+      {
+        src: 'https://alfaonline.servicecdn.ru/public/s3/static/newclick/pwa/icons/android/72_72.png',
+        sizes: '72x72',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: 'https://alfaonline.servicecdn.ru/public/s3/static/newclick/pwa/icons/android/96_96.png',
+        sizes: '96x96',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: 'https://alfaonline.servicecdn.ru/public/s3/static/newclick/pwa/icons/android/144_144.png',
+        sizes: '144x144',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: 'https://alfaonline.servicecdn.ru/public/s3/static/newclick/pwa/icons/android/192_192.png',
         sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: 'https://alfaonline.servicecdn.ru/public/s3/static/newclick/pwa/icons/android/512_512.png',
+        sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
       },
