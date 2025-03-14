3667f085c921fec65065815a1a8bfdd7934847a7
u_m1mkt <sowtame@gmail.com>
2025-03-14 15:16:52 +0700

    feat(deploy_kube_feature): deploy_kube_feature to platform

--- components/setup-worker/index.tsx
+++ components/setup-worker/index.tsx
@@ -1,47 +1,20 @@
 'use client'
 
-import React from 'react'
-// import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 
 export const SetupWorker = () => {
-  // const [first, setFirst] = useState(false)
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setFirst(true)
-  //   }, 1000)
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      registerServiceWorker()
+    }
+  }, [])
 
-  //   console.log(2)
-
-  //   const test = async () => {
-  //     const wakeLock = await navigator.wakeLock.request('screen')
-
-  //     setTimeout(() => {
-  //       wakeLock.release()
-  //     }, 2000)
-
-  //     wakeLock.addEventListener('release', () => {
-  //       console.log('clear')
-  //     })
-  //   }
-
-  //   test()
-
-  //   // if ('serviceWorker' in navigator) {
-  //   //   registerServiceWorker()
-  //   // }
-  // }, [])
-
-  // async function registerServiceWorker() {
-  //   const registration = await navigator.serviceWorker.register('/sw.js', {
-  //     scope: '/',
-  //     updateViaCache: 'none',
-  //   })
-  //   // const sub = await registration.pushManager.getSubscription()
-  // }
-
-  // if (first) {
-  //   return <div>auth</div>
-  // }
+  async function registerServiceWorker() {
+    await navigator.serviceWorker.register('/sw.js', {
+      scope: '/',
+      updateViaCache: 'none',
+    })
+  }
 
   return <div></div>
 }
